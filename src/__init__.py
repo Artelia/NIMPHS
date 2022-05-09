0b80c89612ae442eae0ef7a02b91f7458862d0c1
Félix <felix.olart@arteliagroup.com>
2022-05-09 11:41:01 +0200

    [Utils] Added tests for environment (libs and Blender version)

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,12 @@
 # <pep8 compliant>
+import os
 from . import auto_load
+from bpy.app import version as bl_version
 from bpy.utils import previews
 from bpy.types import Scene, Object
 from bpy.props import PointerProperty
 from bpy.app.handlers import frame_change_pre
-from os import path
 
-from src.operators.openfoam.utils import update_streaming_sequence
-from src.panels.custom_progress_bar import register_custom_progress_bar
-from src.properties.shared.tbb_scene import TBB_Scene
-from src.properties.shared.tbb_object import TBB_Object
 
 bl_info = {
     "name": "Toolsbox OpenFOAM/TELEMAC",
@@ -23,6 +20,49 @@ bl_info = {
     "doc_url": "https://gitlab.arteliagroup.com/water/hydronum/toolsbox_blender/-/wikis/home",
     "tracker_url": "https://gitlab.arteliagroup.com/water/hydronum/toolsbox_blender/-/issues"
 }
+
+# Test environment, inspired from:
+# https://github.com/JacquesLucke/animation_nodes/blob/master/animation_nodes/__init__.py
+# current_directory = os.path.dirname(os.path.abspath(__file__))
+# addons_directory = os.path.dirname(current_directory)
+# addon_name = os.path.basename(current_directory)
+
+try:
+    import numpy
+except BaseException:
+    pass
+
+if "numpy" not in globals():
+    message = ("\n\n"
+               "The Toolsbox Blender addon depends on the numpy library.\n"
+               "Unfortunately the Blender built you are using does not have this library.\n"
+               "Please checkout the documentation to fix this issue (installation guide).")
+    raise Exception(message)
+
+try:
+    import pyvista
+except BaseException:
+    pass
+
+if "pyvista" not in globals():
+    message = ("\n\n"
+               "The Toolsbox Blender addon depends on the pyvista library.\n"
+               "Unfortunately the Blender built you are using does not have this library.\n"
+               "Please checkout the documentation to fix this issue (installation guide).")
+    raise Exception(message)
+
+if bl_version < (3, 0, 0):
+    message = ("\n\n"
+               "The Toolsbox Blender addon requires at least Blender 3.0.\n"
+               "Your are using an older version.\n"
+               "Please download the latest official release.")
+    raise Exception(message)
+
+# Register
+from src.properties.shared.tbb_scene import TBB_Scene
+from src.properties.shared.tbb_object import TBB_Object
+from src.operators.openfoam.utils import update_streaming_sequence
+from src.panels.custom_progress_bar import register_custom_progress_bar
 
 auto_load.init()
 
@@ -42,9 +82,9 @@ def register():
 
     # Add custom icons
     icons = previews.new()
-    icons_dir = path.join(path.dirname(__file__), "icons")
-    icons.load("openfoam", path.join(icons_dir, "openfoam_32_px.png"), 'IMAGE')
-    icons.load("telemac", path.join(icons_dir, "telemac_32_px.png"), 'IMAGE')
+    icons_dir = os.path.join(os.path.dirname(__file__), "icons")
+    icons.load("openfoam", os.path.join(icons_dir, "openfoam_32_px.png"), 'IMAGE')
+    icons.load("telemac", os.path.join(icons_dir, "telemac_32_px.png"), 'IMAGE')
 
     Scene.tbb_icons = {"main": icons}
 
