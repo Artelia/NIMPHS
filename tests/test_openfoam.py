6e020ff6ef08cd67d84a1aa6a200b5331f472ef8
Félix <felix.olart@arteliagroup.com>
2022-05-11 17:31:12 +0200

    [Tests] WIP: writing tests for operators

--- tests/test_openfoam.py
+++ tests/test_openfoam.py
@@ -9,7 +9,7 @@ import bpy
 
 def test_load_openfoam():
     path = os.path.abspath("./data/openfoam_sample_a/foam.foam")
-    bpy.ops.tbb.import_openfoam_file('INVOKE_DEFAULT', filepath=path)
+    assert bpy.ops.tbb.import_openfoam_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
 
     tmp_data = bpy.context.scene.tbb.settings.openfoam.tmp_data
     # Verify default parameters
@@ -19,3 +19,57 @@ def test_load_openfoam():
     assert tmp_data.data is not None
     assert tmp_data.mesh is not None
     assert tmp_data.nb_time_points == 21
+
+
+def test_reload_openfoam():
+    path = os.path.abspath("./data/openfoam_sample_a/foam.foam")
+    assert bpy.ops.tbb.import_openfoam_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
+
+    assert bpy.ops.tbb.reload_openfoam_file('EXEC_DEFAULT') == {"FINISHED"}
+
+
+def test_preview_openfoam():
+    path = os.path.abspath("./data/openfoam_sample_a/foam.foam")
+    assert bpy.ops.tbb.import_openfoam_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
+
+    assert bpy.ops.tbb.openfoam_preview('EXEC_DEFAULT') == {"FINISHED"}
+
+
+def test_create_mesh_sequence_openfoam():
+    path = os.path.abspath("./data/openfoam_sample_a/foam.foam")
+    assert bpy.ops.tbb.import_openfoam_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
+
+    settings = bpy.context.scene.tbb.settings.openfoam
+    # Clip settings
+    settings.clip.type = 'scalar'
+    settings.clip.scalar.name = 'alpha.water@value'
+    settings.clip.scalar.value = 0.5
+    # Sequence settings
+    settings.sequence_type = "mesh_sequence"
+    settings["start_time_poit"] = 0
+    settings["end_time_point"] = 20
+    settings.import_point_data = True
+    settings.list_point_data = "U;alpha.water;p;p_rgh"
+    settings.sequence_name = "My_OpenFOAM_Sim"
+    assert bpy.ops.tbb.openfoam_create_sequence('EXEC_DEFAULT') == {"RUNNING_MODAL"}
+
+
+def test_create_streaming_sequence_openfoam():
+    path = os.path.abspath("./data/openfoam_sample_a/foam.foam")
+    assert bpy.ops.tbb.import_openfoam_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
+
+    settings = bpy.context.scene.tbb.settings.openfoam
+    # Clip settings
+    settings.clip.type = 'scalar'
+    settings.clip.scalar.name = 'alpha.water@value'
+    settings.clip.scalar.value = 0.5
+    # Sequence settings
+    settings.sequence_type = "streaming_sequence"
+    settings.frame_start = 0
+    settings.anim_length = 21
+    settings.import_point_data = True
+    settings.list_point_data = "U;alpha.water;p;p_rgh"
+    settings.sequence_name = "My_OpenFOAM_Streaming_Sim"
+    assert bpy.ops.tbb.openfoam_create_sequence('EXEC_DEFAULT') == {"FINISHED"}
+
+    # Test settings of the created sequence
--- tests/test_telemac.py
+++ tests/test_telemac.py
@@ -8,10 +8,17 @@ import bpy
 # Number of time points = 31
 # Triangulated mesh: Vertices = 12,506 | Edges = 36,704 | Faces = 24,199 | Triangles = 24,199
 
+# Sample B:
+# Number of variables = 6
+# Number of planes = 16
+# Is not from a 3D simulation
+# Number of time points = 31
+# Triangulated mesh: Vertices = 12,506 | Edges = 36,704 | Faces = 24,199 | Triangles = 24,199
 
-def test_load_telemac():
+
+def test_load_telemac_2d():
     path = os.path.abspath("./data/telemac_sample_a.slf")
-    bpy.ops.tbb.import_telemac_file('INVOKE_DEFAULT', filepath=path)
+    assert bpy.ops.tbb.import_telemac_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
 
     tmp_data = bpy.context.scene.tbb.settings.telemac.tmp_data
     # Verify default parameters
@@ -26,3 +33,22 @@ def test_load_telemac():
     assert tmp_data.nb_vertices == 12506
     assert tmp_data.nb_triangles == 24199
     assert not tmp_data.is_3d
+
+
+def test_load_telemac_3d():
+    path = os.path.abspath("./data/telemac_sample_b.slf")
+    assert bpy.ops.tbb.import_telemac_file('EXEC_DEFAULT', filepath=path) == {"FINISHED"}
+
+    tmp_data = bpy.context.scene.tbb.settings.telemac.tmp_data
+    # Verify default parameters
+    assert tmp_data.module_name == "TELEMAC"
+    assert tmp_data.file is not None
+    assert tmp_data.vertices is not None
+    assert tmp_data.faces is not None
+    assert tmp_data.nb_vars == 6
+    assert tmp_data.nb_time_points == 31
+    assert tmp_data.vars_info is not None
+    assert tmp_data.nb_planes == 16
+    assert tmp_data.nb_vertices == 12506
+    assert tmp_data.nb_triangles == 24199
+    assert tmp_data.is_3d
