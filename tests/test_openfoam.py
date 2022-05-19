a58095276b9b48c7eec162abd3d714be8eaae242
Félix <felix.olart@arteliagroup.com>
2022-05-19 16:04:26 +0200

    [Tests] Added pytest fixtures for openfoam

--- tests/test_openfoam.py
+++ tests/test_openfoam.py
@@ -1,6 +1,7 @@
 # <pep8 compliant>
 import os
 import bpy
+import pytest
 
 # Sample A:
 # Number of time points = 21
@@ -11,6 +12,21 @@ import bpy
 # Vertices = 55,718 | Edges = 159,953 | Faces = 104,450 | Triangles = 104,450
 
 FILE_PATH = os.path.abspath("./data/openfoam_sample_a/foam.foam")
+
+
+@pytest.fixture
+def preview_object():
+    return bpy.data.objects.get("TBB_OpenFOAM_preview", None)
+
+
+@pytest.fixture
+def mesh_sequence():
+    return bpy.data.objects.get("My_OpenFOAM_Sim_sequence", None)
+
+
+@pytest.fixture
+def streaming_sequence():
+    return bpy.data.objects.get("My_OpenFOAM_Streaming_Sim_sequence", None)
 
 
 def test_import_openfoam():
@@ -27,14 +43,13 @@ def test_import_openfoam():
     assert tmp_data.nb_time_points == 21
 
 
-def test_geometry_imported_preview_object_openfoam():
-    prw_obj = bpy.data.objects.get("TBB_OpenFOAM_preview", None)
-    assert prw_obj is not None
+def test_geometry_imported_preview_object_openfoam(preview_object):
+    assert preview_object is not None
 
     # Test geometry
-    assert len(prw_obj.data.vertices) == 61616
-    assert len(prw_obj.data.edges) == 182698
-    assert len(prw_obj.data.polygons) == 121092
+    assert len(preview_object.data.vertices) == 61616
+    assert len(preview_object.data.edges) == 182698
+    assert len(preview_object.data.polygons) == 121092
 
 
 def test_reload_openfoam():
@@ -56,17 +71,18 @@ def test_normal_preview_object_openfoam():
     settings.decompose_polyhedra = False
     settings.triangulate = False
     settings.case_type = '0'
+    settings.preview_point_data = "alpha.water@value"
 
     assert bpy.ops.tbb.openfoam_preview('EXEC_DEFAULT') == {"FINISHED"}
 
 
-def test_geometry_normal_preview_object():
-    prw_obj = bpy.data.objects.get("TBB_OpenFOAM_preview", None)
-    assert prw_obj is not None
+def test_geometry_normal_preview_object(preview_object):
+    assert preview_object is not None
+
     # Test geometry
-    assert len(prw_obj.data.vertices) == 60548
-    assert len(prw_obj.data.edges) == 120428
-    assert len(prw_obj.data.polygons) == 59882
+    assert len(preview_object.data.vertices) == 60548
+    assert len(preview_object.data.edges) == 120428
+    assert len(preview_object.data.polygons) == 59882
 
 
 def test_normal_decompose_polyhedra_preview_object_openfoam():
@@ -75,17 +91,18 @@ def test_normal_decompose_polyhedra_preview_object_openfoam():
     settings.decompose_polyhedra = True
     settings.triangulate = False
     settings.case_type = '0'
+    settings.preview_point_data = "alpha.water@value"
 
     assert bpy.ops.tbb.openfoam_preview('EXEC_DEFAULT') == {"FINISHED"}
 
 
-def test_geometry_normal_decompose_polyhedra_preview_object_openfoam():
-    prw_obj = bpy.data.objects.get("TBB_OpenFOAM_preview", None)
-    assert prw_obj is not None
+def test_geometry_normal_decompose_polyhedra_preview_object_openfoam(preview_object):
+    assert preview_object is not None
+
     # Test geometry
-    assert len(prw_obj.data.vertices) == 60548
-    assert len(prw_obj.data.edges) == 121748
-    assert len(prw_obj.data.polygons) == 61202
+    assert len(preview_object.data.vertices) == 60548
+    assert len(preview_object.data.edges) == 121748
+    assert len(preview_object.data.polygons) == 61202
 
 
 def test_triangulated_preview_object_openfoam():
@@ -94,39 +111,21 @@ def test_triangulated_preview_object_openfoam():
     settings.decompose_polyhedra = False
     settings.triangulate = True
     settings.case_type = '0'
+    settings.preview_point_data = "alpha.water@value"
 
     assert bpy.ops.tbb.openfoam_preview('EXEC_DEFAULT') == {"FINISHED"}
 
 
-def test_geometry_triangulated_preview_object_openfoam():
-    prw_obj = bpy.data.objects.get("TBB_OpenFOAM_preview", None)
-    assert prw_obj is not None
+def test_geometry_triangulated_preview_object_openfoam(preview_object):
+    assert preview_object is not None
+
     # Test geometry
-    assert len(prw_obj.data.vertices) == 61616
-    assert len(prw_obj.data.edges) == 182698
-    assert len(prw_obj.data.polygons) == 121092
+    assert len(preview_object.data.vertices) == 61616
+    assert len(preview_object.data.edges) == 182698
+    assert len(preview_object.data.polygons) == 121092
 
 
 def test_triangulated_decompose_polyhedra_preview_object_openfoam():
-    # Set preview settings
-    settings = bpy.context.scene.tbb.settings.openfoam
-    settings.decompose_polyhedra = True
-    settings.triangulate = True
-    settings.case_type = '0'
-
-    assert bpy.ops.tbb.openfoam_preview('EXEC_DEFAULT') == {"FINISHED"}
-
-
-def test_geometry_triangulated_decompose_polyhedra_preview_object_openfoam():
-    prw_obj = bpy.data.objects.get("TBB_OpenFOAM_preview", None)
-    assert prw_obj is not None
-    # Test geometry
-    assert len(prw_obj.data.vertices) == 61616
-    assert len(prw_obj.data.edges) == 182698
-    assert len(prw_obj.data.polygons) == 121092
-
-
-def test_point_data_preview_object_openfoam():
     # Set preview settings
     settings = bpy.context.scene.tbb.settings.openfoam
     settings.decompose_polyhedra = True
@@ -136,11 +135,21 @@ def test_point_data_preview_object_openfoam():
 
     assert bpy.ops.tbb.openfoam_preview('EXEC_DEFAULT') == {"FINISHED"}
 
-    prw_obj = bpy.data.objects.get("TBB_OpenFOAM_preview", None)
-    assert prw_obj is not None
+
+def test_geometry_triangulated_decompose_polyhedra_preview_object_openfoam(preview_object):
+    assert preview_object is not None
+
+    # Test geometry
+    assert len(preview_object.data.vertices) == 61616
+    assert len(preview_object.data.edges) == 182698
+    assert len(preview_object.data.polygons) == 121092
+
+
+def test_point_data_preview_object_openfoam(preview_object):
+    assert preview_object is not None
 
     # Test point data (only test if they exist)
-    vertex_colors = prw_obj.data.vertex_colors
+    vertex_colors = preview_object.data.vertex_colors
     assert len(vertex_colors) == 1
     aw_colors = vertex_colors.get("alpha.water, None, None", None)
     assert aw_colors is not None
@@ -190,13 +199,12 @@ def test_create_streaming_sequence_openfoam():
     assert bpy.ops.tbb.openfoam_create_sequence('EXEC_DEFAULT') == {"FINISHED"}
 
 
-def test_streaming_sequence_openfoam():
-    seq_obj = bpy.data.objects.get("My_OpenFOAM_Streaming_Sim_sequence", None)
-    assert seq_obj is not None
-    assert seq_obj.tbb.is_streaming_sequence == True
+def test_streaming_sequence_openfoam(streaming_sequence):
+    assert streaming_sequence is not None
+    assert streaming_sequence.tbb.is_streaming_sequence == True
 
     # Test common settings
-    seq_settings = seq_obj.tbb.settings.openfoam.streaming_sequence
+    seq_settings = streaming_sequence.tbb.settings.openfoam.streaming_sequence
     assert seq_settings.name == "My_OpenFOAM_Streaming_Sim_sequence"
     assert seq_settings.update == True
     assert seq_settings.file_path == FILE_PATH
@@ -216,27 +224,25 @@ def test_streaming_sequence_openfoam():
     assert seq_settings.clip.scalar.invert == False
 
 
-def test_geometry_streaming_sequence_openfoam():
-    seq_obj = bpy.data.objects.get("My_OpenFOAM_Streaming_Sim_sequence", None)
-    assert seq_obj is not None
+def test_geometry_streaming_sequence_openfoam(streaming_sequence):
+    assert streaming_sequence is not None
 
     # Change frame to load another time point for the streaming sequence
     bpy.context.scene.frame_set(11)
     # Disable updates for this sequence object during the next tests
-    seq_obj.tbb.settings.openfoam.streaming_sequence.update = False
+    streaming_sequence.tbb.settings.openfoam.streaming_sequence.update = False
 
     # Test geometry (tp 11, clip on alpha.water, 0.5, triangulated, decompose polyhedra)
-    assert len(seq_obj.data.vertices) == 55718
-    assert len(seq_obj.data.edges) == 159953
-    assert len(seq_obj.data.polygons) == 104450
+    assert len(streaming_sequence.data.vertices) == 55718
+    assert len(streaming_sequence.data.edges) == 159953
+    assert len(streaming_sequence.data.polygons) == 104450
 
 
-def test_point_data_streaming_sequence_openfoam():
-    seq_obj = bpy.data.objects.get("My_OpenFOAM_Streaming_Sim_sequence", None)
-    assert seq_obj is not None
+def test_point_data_streaming_sequence_openfoam(streaming_sequence):
+    assert streaming_sequence is not None
 
     # Test point data (only test if they exist)
-    vertex_colors = seq_obj.data.vertex_colors
+    vertex_colors = streaming_sequence.data.vertex_colors
     assert len(vertex_colors) == 2
     u_colors = vertex_colors.get("U.x, U.y, U.z", None)
     assert u_colors is not None
@@ -267,47 +273,44 @@ def test_create_mesh_sequence_openfoam():
     assert bpy.ops.tbb.openfoam_create_sequence('EXEC_DEFAULT', mode='NORMAL') == {"FINISHED"}
 
 
-def test_mesh_sequence_openfoam():
-    seq_obj = bpy.data.objects.get("My_OpenFOAM_Sim_sequence", None)
-    assert seq_obj is not None
+def test_mesh_sequence_openfoam(mesh_sequence):
+    assert mesh_sequence is not None
 
     # Change frame to load another time point for the mesh sequence
     bpy.context.scene.frame_set(11)
 
     # Test mesh sequence (settings from Stop-Motion-OBJ)
-    assert seq_obj.mesh_sequence_settings.numMeshes == 12
-    assert seq_obj.mesh_sequence_settings.numMeshesInMemory == 12
-    assert seq_obj.mesh_sequence_settings.startFrame == 1
-    assert seq_obj.mesh_sequence_settings.speed == 1.0
-    assert seq_obj.mesh_sequence_settings.streamDuringPlayback == True
-    assert seq_obj.mesh_sequence_settings.showAsSingleMesh == False
-    assert seq_obj.mesh_sequence_settings.perFrameMaterial == False
-    assert seq_obj.mesh_sequence_settings.loaded == True
-    assert seq_obj.mesh_sequence_settings.initialized == True
-    assert seq_obj.mesh_sequence_settings.name == ""
-    assert seq_obj.mesh_sequence_settings.meshNames == ""
-    assert seq_obj.mesh_sequence_settings.meshNameArray is not None
-    assert seq_obj.mesh_sequence_settings.frameMode == '4'
-    assert seq_obj.mesh_sequence_settings.isImported == False
-    assert seq_obj.mesh_sequence_settings.fileName == ""
+    assert mesh_sequence.mesh_sequence_settings.numMeshes == 12
+    assert mesh_sequence.mesh_sequence_settings.numMeshesInMemory == 12
+    assert mesh_sequence.mesh_sequence_settings.startFrame == 1
+    assert mesh_sequence.mesh_sequence_settings.speed == 1.0
+    assert mesh_sequence.mesh_sequence_settings.streamDuringPlayback == True
+    assert mesh_sequence.mesh_sequence_settings.showAsSingleMesh == False
+    assert mesh_sequence.mesh_sequence_settings.perFrameMaterial == False
+    assert mesh_sequence.mesh_sequence_settings.loaded == True
+    assert mesh_sequence.mesh_sequence_settings.initialized == True
+    assert mesh_sequence.mesh_sequence_settings.name == ""
+    assert mesh_sequence.mesh_sequence_settings.meshNames == ""
+    assert mesh_sequence.mesh_sequence_settings.meshNameArray is not None
+    assert mesh_sequence.mesh_sequence_settings.frameMode == '4'
+    assert mesh_sequence.mesh_sequence_settings.isImported == False
+    assert mesh_sequence.mesh_sequence_settings.fileName == ""
 
 
-def test_geometry_mesh_sequence_openfoam():
-    seq_obj = bpy.data.objects.get("My_OpenFOAM_Sim_sequence", None)
-    assert seq_obj is not None
+def test_geometry_mesh_sequence_openfoam(mesh_sequence):
+    assert mesh_sequence is not None
 
     # Test geometry (tp 11, clip on alpha.water, 0.5, triangulated, decompose polyhedra)
-    assert len(seq_obj.data.vertices) == 55718
-    assert len(seq_obj.data.edges) == 159953
-    assert len(seq_obj.data.polygons) == 104450
+    assert len(mesh_sequence.data.vertices) == 55718
+    assert len(mesh_sequence.data.edges) == 159953
+    assert len(mesh_sequence.data.polygons) == 104450
 
 
-def test_point_data_mesh_sequence_openfoam():
-    seq_obj = bpy.data.objects.get("My_OpenFOAM_Sim_sequence", None)
-    assert seq_obj is not None
+def test_point_data_mesh_sequence_openfoam(mesh_sequence):
+    assert mesh_sequence is not None
 
     # Test point data (only test if they exist)
-    vertex_colors = seq_obj.data.vertex_colors
+    vertex_colors = mesh_sequence.data.vertex_colors
     assert len(vertex_colors) == 2
     u_colors = vertex_colors.get("U.x, U.y, U.z", None)
     assert u_colors is not None
